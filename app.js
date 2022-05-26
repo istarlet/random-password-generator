5a9478ebda303d18302ecf295d346965fe7486e6
istarlet <istarlet@gmail.com>
2022-05-26 00:47:31 -0400

    Start writing JS

---
+++ app.js
@@ -0,0 +1,9 @@
+// Get the DOM elements
+const generatePasswords = document.getElementById("generate-passwords");
+
+function createPasswords() {
+
+}
+
+// Listen for clicks on the button;
+generatePasswords.addEventListener("click", createPasswords);
